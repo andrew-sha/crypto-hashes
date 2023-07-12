fc2391c3d3c574d7c97136ed1453ebaa0bbcf60b
Andrew Shannon <a.shannon9@gmail.com>
2023-07-11 23:14:00 -0400

    type hinting and removing lines

--- util.py
+++ util.py
@@ -1,122 +1,93 @@
 import constants
+from typing import List
 import hashlib
 
 # bit operations for bit strings
-def AND(x, y):
-    s = ''
-    if len(x) != len(y):
-        print('Bit strings are of different length.')
-    else:
-        for i in range(len(x)):
-            s += str(int(x[i]) & int(y[i]))
-        return(s)
+def AND(x: str, y: str) -> str:
+    try:
+        return ''.join([str(int(x[i]) & int(y[i])) for i in range(max(len(x), len(y)))])
+    except IndexError:
+        print('Inputs are of different length')
 
-def OR(x, y):
-    s = ''
-    if len(x) != len(y):
-        print('Bit strings are of different length.')
-    else:
-        for i in range(len(x)):
-            s += str(int(x[i]) | int(y[i]))
-        return(s)
+def OR(x: str, y: str) -> str:
+    try:
+        return ''.join([str(int(x[i]) | int(y[i])) for i in range(max(len(x), len(y)))])
+    except IndexError:
+        print('Inputs are of different length')
 
-def XOR(x, y):
-    s = ''
-    if len(x) != len(y):
-        print('Bit strings are of different length.')
-    else:
-        for i in range(len(x)):
-            s += str(int(x[i]) ^ int(y[i]))
-        return(s) 
+def XOR(x: str, y: str) -> str:
+    try:
+        return ''.join([str(int(x[i]) ^ int(y[i])) for i in range(max(len(x), len(y)))])
+    except IndexError:
+        print('Inputs are of different length')
 
-def NOT(x):
-    s = ''
-    for i in x:
-        s += str((1 + int(i)) % 2)
-    return s
+def NOT(x: str) -> str:
+    return ''.join([str((1 + int(i)) % 2) for i in x])
 
-def lshift(x, n):
+def lshift(x: str, n: int) -> str:
     # <<
     if n > len(x):
         return 'Shift amount exceeds string length'
     return x[n:] + ('0' * n)
 
-def rshift(x, n):
+def rshift(x: str, n: int) -> str:
     # >>
     if n > len(x):
         return 'Shift amount exceeds string length' 
     return ('0' * n) +  x[:len(x)-n]
 
-def bitadd(x, y, p):
+def bitadd(x: str, y: str, p: int) -> str:
     # add the strings x and y modulo 2^p
-    x_base_ten = int(x, 2)
-    y_base_ten = int(y, 2)
-    sum = (x_base_ten + y_base_ten) % (2 ** p)
-    return '0' * (len(x)-len(bin(sum)[2:])) + bin(sum)[2:]
+    sum = (int(x, 2) + int(y, 2)) % (2 ** p)
+    return bin(sum)[2:].zfill(len(x))
+
 
 # subroutines used in SHA256
-def Ch(x, y, z):
+def Ch(x: str, y: str, z: str) -> str:
     return XOR(AND(x,y), AND(NOT(x), z)) 
 
-def Maj(x, y, z):
+def Maj(x: str, y: str, z: str) -> str:
     return XOR(XOR(AND(x,y), AND(x,z)), AND(y, z))
 
-def ROTR(x, n):
-    '''
-    Circular right shift
-    x is an w-bit word and n is an integer 0 <= n < w
-    '''
+def ROTR(x: str, n: int) -> str:
+    # circular right shift
     if len(x) <= n:
         return 'Shift amount exceeds string length'
     return OR(rshift(x, n), lshift(x, len(x) - n))
 
-def SIGMA_0(x):
+def SIGMA_0(x: str) -> str:
     return XOR(XOR(ROTR(x, 2), ROTR(x, 13)), ROTR(x, 22))
 
-def SIGMA_1(x):
+def SIGMA_1(x: str) -> str:
     return XOR(XOR(ROTR(x, 6), ROTR(x, 11)), ROTR(x, 25)) 
 
-def sigma_0(x):
+def sigma_0(x: str) -> str:
     return XOR(XOR(ROTR(x, 7), ROTR(x, 18)), rshift(x, 3))
 
-def sigma_1(x):
+def sigma_1(x: str) -> str:
     return XOR(XOR(ROTR(x, 17), ROTR(x, 19)), rshift(x, 10))
 
 # functions for preprocessing message
-def encode(m):
-    '''
-    unicode message
-    '''
-    encoded_message = ''
-    for char in m:
-        encoded_message += bin(ord(char))[2:].zfill(8)
-    return encoded_message
+def encode(m: str) -> str:
+    # unicode message
+    return ''.join([bin(ord(char))[2:].zfill(8) for char in m])
 
-def pad_message(m):
-    '''
-    m is a bit string 
-    convert the string m into a bit string whose length is a multiple of 512
-    '''
+def pad_message(m: str) -> str:
+    # convert the bit string m into a bit string whose length is a multiple of 512
     padded = m + '1'
     while (len(padded) % 512) != 448:
         padded += '0'
     return padded + bin(len(m))[2:].zfill(64)
 
-
-def parse_message(m, k):
-    '''
-    m is a padded bit string whose length is a multiple of k
-    break the padded message into a list of N k-bit blocks
-    '''
+def parse_message(m: str, k: int) -> List[str]:
+    # break the bit string m into a list of N k-bit blocks
     N = int(len(m) / k)
     return [m[i * k: (i + 1) * k] for i in range(N)]
 
 
 # functions for computing the hash
-def generate_schedule(block):
-    '''
-    block is a 512-bit block that will be converted into a schedule during one iteration of the hash computation
-    '''
+def generate_schedule(block: str) -> List[str]:
+    # convert a 512 bit block into a schedule during one iteration of the hash computation
     schedule = []
     words = parse_message(block, 32)
     for t in range(64):
@@ -127,13 +98,7 @@ def generate_schedule(block):
 
     return schedule
 
-def compute_hash(m):
-    '''
-    m is the message to be hashed
-    '''
-    # initialize working variables for first iteration
-    #working_variables = [bin(h)[2:].zfill(32)  for h in constants.INITIAL_HASHES]
-
+def compute_hash(m: str) -> List[str]:
     # parse message into blocks
     blocks = parse_message(pad_message(m), 512)
 
@@ -176,7 +141,7 @@ def compute_hash(m):
     return hashes
 
 
-def SHA256(m):
+def SHA256(m: str) -> str:
     # encode message using unicode
     digest = compute_hash(encode(m))
     
@@ -185,14 +150,3 @@ def SHA256(m):
         digest[i] = hex(int(digest[i], 2))[2:].zfill(8)
     
     return ''.join(digest)
-
-if __name__ == '__main__':
-    long = 'a' * 1000
-    test_vectors = [
-    'abc',
-    '',
-    'abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq',
-    'abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu',
-    long]
-    for word in test_vectors:
-        print(SHA256(word) == hashlib.sha256(word.encode()).hexdigest())
