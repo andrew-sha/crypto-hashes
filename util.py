4cec6a49f997a19a5fea8b3af7b56f4cf922506e
Andrew Shannon <a.shannon9@gmail.com>
2023-07-17 23:27:02 -0400

    major overhaul, much faster overall

--- util.py
+++ util.py
@@ -2,108 +2,88 @@ import constants
 from typing import List
 import hashlib
 
-# bit operations for bit strings
-def AND(x: str, y: str) -> str:
-    try:
-        return ''.join([str(int(x[i]) & int(y[i])) for i in range(max(len(x), len(y)))])
-    except IndexError:
-        print('Inputs are of different length')
 
-def OR(x: str, y: str) -> str:
-    try:
-        return ''.join([str(int(x[i]) | int(y[i])) for i in range(max(len(x), len(y)))])
-    except IndexError:
-        print('Inputs are of different length')
+MODULUS = 2**32
 
-def XOR(x: str, y: str) -> str:
-    try:
-        return ''.join([str(int(x[i]) ^ int(y[i])) for i in range(max(len(x), len(y)))])
-    except IndexError:
-        print('Inputs are of different length')
+'''
+SUBROUTINES
+'''
 
-def NOT(x: str) -> str:
-    return ''.join([str((1 + int(i)) % 2) for i in x])
+'''SHA256'''
+def Ch(x, y, z):
+    # if x then y else z
+    return (x & y) ^ (~x & z)
 
-def lshift(x: str, n: int) -> str:
-    # <<
-    if n > len(x):
-        return 'Shift amount exceeds string length'
-    return x[n:] + ('0' * n)
+def Maj(x, y, z):
+    return (x & y) ^ (x & z) ^ (y & z)
 
-def rshift(x: str, n: int) -> str:
-    # >>
-    if n > len(x):
-        return 'Shift amount exceeds string length' 
-    return ('0' * n) +  x[:len(x)-n]
-
-def bitadd(x: str, y: str, p: int) -> str:
-    # add the strings x and y modulo 2^p
-    sum = (int(x, 2) + int(y, 2)) % (2 ** p)
-    return bin(sum)[2:].zfill(len(x))
-
-
-# subroutines used in SHA256
-def Ch(x: str, y: str, z: str) -> str:
-    return XOR(AND(x,y), AND(NOT(x), z)) 
-
-def Maj(x: str, y: str, z: str) -> str:
-    return XOR(XOR(AND(x,y), AND(x,z)), AND(y, z))
-
-def ROTR(x: str, n: int) -> str:
+def ROTR(x, n: int):
     # circular right shift
-    if len(x) <= n:
-        return 'Shift amount exceeds string length'
-    return OR(rshift(x, n), lshift(x, len(x) - n))
+    return (x >> n) | (x << (32 - n))
 
-def SIGMA_0(x: str) -> str:
-    return XOR(XOR(ROTR(x, 2), ROTR(x, 13)), ROTR(x, 22))
+def SIGMA_0(x):
+    return ROTR(x, 2) ^ ROTR(x, 13) ^ ROTR(x, 22)
 
-def SIGMA_1(x: str) -> str:
-    return XOR(XOR(ROTR(x, 6), ROTR(x, 11)), ROTR(x, 25)) 
+def SIGMA_1(x):
+    return ROTR(x, 6) ^ ROTR(x, 11) ^ ROTR(x, 25)
 
-def sigma_0(x: str) -> str:
-    return XOR(XOR(ROTR(x, 7), ROTR(x, 18)), rshift(x, 3))
+def sigma_0(x):
+    return ROTR(x, 7) ^ ROTR(x, 18) ^ (x >> 3)
 
-def sigma_1(x: str) -> str:
-    return XOR(XOR(ROTR(x, 17), ROTR(x, 19)), rshift(x, 10))
+def sigma_1(x):
+    return ROTR(x, 17) ^ ROTR(x, 19) ^ (x >> 10)
 
-# functions for preprocessing message
-def encode(m: str) -> str:
-    # unicode message
-    return ''.join([bin(ord(char))[2:].zfill(8) for char in m])
+'''MD5'''
 
+'''
+PREPROCESSING
+'''
 def pad_message(m: str) -> str:
-    # convert the bit string m into a bit string whose length is a multiple of 512
-    padded = m + '1'
-    while (len(padded) % 512) != 448:
-        padded += '0'
-    return padded + bin(len(m))[2:].zfill(64)
+    # pad message such that its length in bits is a multiple 512
+
+    # get number of bits in original message
+    original_length = len(m) * 8
+
+    # append the byte-equivalent of '10000000' to the end of the message
+    m += b'\x80'
+
+    # append zeros until length of message in bits is equal to 448 mod 512
+    while ((len(m) * 8) % 512) != 448:
+        m += b'\x00'
+
+    return (m + original_length.to_bytes(8, 'big'))
 
 def parse_message(m: str, k: int) -> List[str]:
-    # break the bit string m into a list of N k-bit blocks
+    # break a string (e.g. sequence of bytes) into blocks of size k
     N = int(len(m) / k)
     return [m[i * k: (i + 1) * k] for i in range(N)]
 
-
-# functions for computing the hash
 def generate_schedule(block: str) -> List[str]:
-    # convert a 512 bit block into a schedule during one iteration of the hash computation
+    # convert a 64 byte (512 bit) block into a schedule during one iteration of the hash computation
     schedule = []
-    words = parse_message(block, 32)
+    words = parse_message(block, 4)
     for t in range(64):
         if t <= 15:
             schedule.append(words[t])
         if t > 15:
-            schedule.append(bitadd(bitadd(sigma_1(schedule[t-2]), schedule[t-7], 32), bitadd(sigma_0(schedule[t-15]), schedule[t-16], 32), 32))
+            s0 = sigma_1(int.from_bytes(schedule[t-2], 'big')) 
+            s1 = int.from_bytes(schedule[t-7], 'big')
+            s2 = sigma_0(int.from_bytes(schedule[t-15], 'big'))
+            s3 = int.from_bytes(schedule[t-16], 'big')
+            s4 = (s0 + s1 + s2 + s3) % MODULUS
+            schedule.append(s4.to_bytes(4, 'big'))
 
     return schedule
 
-def compute_hash(m: str) -> List[str]:
-    # parse message into blocks
-    blocks = parse_message(pad_message(m), 512)
+'''
+HASH COMPUTATIONS
+'''
+def compute_sha256(m: str) -> List[str]:
+    # parse message into 64 byte (512 bit) blocks
+    blocks = parse_message(pad_message(m), 64)
 
-    # initialize intermediate hash values
-    hashes = [bin(h)[2:].zfill(32) for h in constants.INITIAL_HASHES]
+    # initialize hash values
+    hashes = constants.INITIAL_HASHES_SHA256
 
     # main loop
     for i in range(len(blocks)):
@@ -111,42 +91,94 @@ def compute_hash(m: str) -> List[str]:
         schedule = generate_schedule(blocks[i])
 
         # initialize working variables
-        a = hashes[0]
-        b = hashes[1]
-        c = hashes[2]
-        d = hashes[3]
-        e = hashes[4]
-        f = hashes[5]
-        g = hashes[6]
-        h = hashes[7]
+        [a, b, c, d, e, f, g, h] = [h for h in hashes]
 
         # update working variables
         for t in range(64):
-            T_1 = bitadd(bitadd(bitadd(bitadd(h, SIGMA_1(e), 32), Ch(e, f, g), 32), bin(constants.CONSTANTS[t])[2:].zfill(32) , 32), schedule[t], 32)
-            T_2 = bitadd(SIGMA_0(a), Maj(a, b, c), 32)
+            T_1 = (h + SIGMA_1(e) + Ch(e, f, g) + constants.CONSTANTS_SHA256[t] + int.from_bytes(schedule[t], 'big')) % MODULUS
+            T_2 = (SIGMA_0(a) + Maj(a, b, c)) % MODULUS
             h = g
             g = f
             f = e
-            e = bitadd(d, T_1, 32)
+            e = (d + T_1) % MODULUS
             d = c
             c = b
             b = a
-            a = bitadd(T_1, T_2, 32)
+            a = (T_1 + T_2) % MODULUS
         
-        # update intermediate hashes
+        # update hashes
         working_variables = [a, b, c, d, e, f, g, h]
         for j in range(len(hashes)):
-            hashes[j] = bitadd(working_variables[j], hashes[j], 32)
+            hashes[j] = (working_variables[j] + hashes[j]) % MODULUS
 
-    return hashes
+    # return list of final hashes as bytes
+    return [h.to_bytes(4, 'big') for h in hashes]
+
+''' STILL UNDER WORKS
+def compute_md5(m: str) -> List[str]:
+    # parse message into blocks
+    blocks = parse_message(pad_message(m), 512)
+
+    # initialize intermediate hash values
+    A = bin(constants.INITIAL_HASHES_MD5_2[0])[2:].zfill(32)
+    B = bin(constants.INITIAL_HASHES_MD5_2[1])[2:].zfill(32)
+    C = bin(constants.INITIAL_HASHES_MD5_2[2])[2:].zfill(32)
+    D = bin(constants.INITIAL_HASHES_MD5_2[3])[2:].zfill(32)
+
+    # main loop
+    for i in range(len(blocks)):
+        # break block into 32-bit words
+        schedule = parse_message(blocks[i], 32)
+
+        # initialize working variables
+        AA = A
+        BB = B
+        CC = C
+        DD = D
+        
+        # update working variables
+        for j in range(64):
+            if j in range(16):
+                E = F(BB, CC, DD)
+                k = j
+            elif j in range(16, 32):
+                E = G(BB, CC, DD)
+                k = (j * 5 + 1) % 16
+            elif j in range(32, 48):
+                E = H(BB, CC, DD)
+                k = (j * 3 + 5) % 16
+            elif j in range(48, 64):
+                E = I(BB, CC, DD)
+                k = (j * 7) % 16
+        
+            E = bitadd(E, bitadd(AA, bitadd(bin(constants.CONSTANTS_MD5[j])[2:].zfill(32), schedule[k], 32), 32), 32)
+            AA = DD
+            DD = CC
+            CC = BB
+            BB = bitadd(BB, ROTR(E, 32 - constants.SHIFTS_MD5[j]), 32)
+            '''
+            T = DD
+            DD = CC
+            CC = BB
+            BB = bitadd(BB, bitadd(E, bitadd(AA, bitadd(bin(constants.CONSTANTS_MD5[j])[2:].zfill(32), schedule[k], 32), 32), 32), 32)
+            AA = T
+            '''
+        
+        A = bitadd(A, AA, 32)
+        B = bitadd(B, BB, 32)
+        C = bitadd(C, CC, 32)
+        D = bitadd(D, DD, 32)
+    
+    return [A, B, C, D]
+'''
 
 
 def SHA256(m: str) -> str:
     # encode message using unicode
-    digest = compute_hash(encode(m))
-    
-    # convert digest from sequence of 32-bit blocks to string of 64 hex characters
-    for i in range(len(digest)):
-        digest[i] = hex(int(digest[i], 2))[2:].zfill(8)
-    
-    return ''.join(digest)
+    digest = compute_sha256(m.encode())    
+    return b''.join(digest).hex()
+
+if __name__ == '__main__':
+    m = ''
+    print(SHA256(m))
+    print(hashlib.sha256(m.encode()).hexdigest())
